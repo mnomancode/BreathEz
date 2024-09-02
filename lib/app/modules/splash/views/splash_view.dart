6727aeab39ed7eb1e1f16b4a43676e416dd74493
noman9k <chnoman503@gmail.com>
2024-09-03 00:18:32 +0500

    getx added

---
+++ lib/app/modules/splash/views/splash_view.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../../../routes/app_pages.dart';
+import '../controllers/splash_controller.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.toNamed(Routes.HOME);
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/logo.png'),
+      ),
+    );
+  }
+}
